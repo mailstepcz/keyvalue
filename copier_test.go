3ce6c900865773f5a00126670e203081bcf87d4d
Adam Sitavanc <work@sitavanc.cz>
2026-04-16 10:54:07 +0200

    fix: use index-based pointer arithmetic in slice copy to pass checkptr

--- copier_test.go
+++ copier_test.go
@@ -1038,7 +1038,7 @@ func TestValConv(t *testing.T) {
 
 		var (
 			dst int
-			src int = 1234
+			src = 1234
 		)
 		f, err := valConv(reflect.TypeOf(dst), reflect.TypeOf(src))
 		req.NoError(err)
@@ -1052,7 +1052,7 @@ func TestValConv(t *testing.T) {
 
 		var (
 			dst *int
-			src *int = pointer.To(1234)
+			src = pointer.To(1234)
 		)
 		f, err := valConv(reflect.TypeOf(dst), reflect.TypeOf(src))
 		req.NoError(err)
@@ -1067,7 +1067,7 @@ func TestValConv(t *testing.T) {
 		type alias int
 		var (
 			dst alias
-			src int = 1234
+			src = 1234
 		)
 		f, err := valConv(reflect.TypeOf(dst), reflect.TypeOf(src))
 		req.NoError(err)
@@ -1167,7 +1167,7 @@ func TestValConv(t *testing.T) {
 		u := ulid.Make()
 		var (
 			dst maybe.Maybe[ulid.ULID]
-			src *string = pointer.To(u.String())
+			src = pointer.To(u.String())
 		)
 		f, err := valConv(reflect.TypeOf(dst), reflect.TypeOf(src))
 		req.NoError(err)
@@ -1181,7 +1181,7 @@ func TestValConv(t *testing.T) {
 
 		var (
 			dst maybe.Maybe[decimal.Decimal]
-			src *string = pointer.To("1234")
+			src = pointer.To("1234")
 		)
 		f, err := valConv(reflect.TypeOf(dst), reflect.TypeOf(src))
 		req.NoError(err)
